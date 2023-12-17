32b6911d4aac34075740b8a26543c813931f024b
dalaixlmao <anubhav_a@me.iitr.ac.in>
2023-12-17 19:06:09 +0530

    debugg 2

--- codeforces-api-visualizer-backend/server.js
+++ codeforces-api-visualizer-backend/server.js
@@ -1,35 +1,39 @@
 // server.js
 
-
-
-const express = require('express');
-const axios = require('axios');
-const cors = require('cors'); // Import the cors middleware
+const express = require("express");
+const axios = require("axios");
+const cors = require("cors"); // Import the cors middleware
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors()); // Use cors middleware
 
-app.post('/codeforces', async (req, res) => {
+app.post("/codeforces", async (req, res) => {
   try {
     const { handle } = req.body;
     const codeforcesData = await getCodeforcesData(handle);
     res.json(codeforcesData);
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: 'Internal Server Error' });
+    res.status(500).json({ error: "Internal Server Error" });
   }
+});
+
+app.use("/", (req, res) => {
+  res.json({ message: "everything is fine. Server is on track <3." });
 });
 
 async function getCodeforcesData(handle) {
   try {
-    const response = await axios.get(`https://codeforces.com/api/user.info?handles=${handle}`);
+    const response = await axios.get(
+      `https://codeforces.com/api/user.info?handles=${handle}`
+    );
     console.log(response.data);
     return response.data;
   } catch (error) {
     console.error(error);
-    throw new Error('Failed to fetch Codeforces data');
+    throw new Error("Failed to fetch Codeforces data");
   }
 }
 
